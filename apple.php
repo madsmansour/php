912e14d50a18312cd3c5e748778b3cd3e5fcf39c
madsmansour <42993941+madsmansour@users.noreply.github.com>
2020-02-04 16:15:58 +0100

    php

---
+++ apple.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Æblegrød</title>
+</head>
+<body>
+
+<h1>Æblegrødmaskine</h1>
+
+<p class="intro"><a href="http://www.dk-kogebogen.dk/opskrifter/visopskrift.php?id=2872">Æblegrød-opskrift</a> fra Dk-Kogebogen.</p>
+
+<form onsubmit="return false" oninput="output.value = antalPersoner.valueAsNumber">
+  Hvor mange personer vil du lave grød til?<br />
+  <input id="antalPersoner" type="range" min="1" max="100" value="4"><br /> Antal personer er <output for="antalPersoner" name="output">4</output>.</form>
+
+<ul>
+  <li><input type="text" name="firstname" value="0.8">kg Æbler</li>
+  <li><input type="text" name="firstname" value="0.5">l Vand</li>
+  <li><input type="text" name="firstname" value="0.15">kg Sukker</li>
+  <li>Vanillesukker</li>
+</ul>
+
+<p>Æblerne vaskes og skrælles, kernehuset fjernes, og æblerne snittes i små stykker.</p>
+
+<p>Æblerne sættes over med vandet, dampes møre over svag varme og smages til med sukker.</p>
+
+<p>Serveres med mælk eller fløde.</p>
+    
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHP</title>
+</head>
+<body>
+<?php
+
+$A = $_GET["a"];
+
+$B = $_GET["b"];
+
+$operation = $_GET["operation"];
+
+switch ($operation) {
+    case "plus";
+        echo $A + $B;
+        break;
+    case "minus";
+        echo $A - $B;
+} 
+
+?>
+
+</body>
+</html>
